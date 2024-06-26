7669096540e89303f78b95079c1cceceec9e0659
Sarah <Sarah@cimiytech.com>
2024-06-26 15:34:21 +0800

    Add MUL function

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -11,13 +11,19 @@ class calculator_api:
 
     def op_sub(self):
         return self.a - self.b
+    
+    def op_mul(self):
+        return self.a * self.b
 
 
 if __name__ == '__main__':
     obj_cal = calculator_api(3, 4)
     a = obj_cal.op_add()
     b = obj_cal.op_sub()
+    c = obj_cal.op_mul()
     print (f'Result ADD={a}')
-    print (f'Result DUB={b}')
+    print (f'Result SUB={b}')
+    print (f'Result MUL={c}')
+
 
 
