7e9b7cf0b210608d4c61d0cc371e105c5f731428
Cimiy-Chan <r13338@gmail.com>
2024-06-26 16:16:46 +0800

    Calculator.py modified

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -2,28 +2,27 @@
 import os
 
 class calculator_api:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+    def __init__(self):
+        pass
 
-    def op_add(self):
-        return self.a + self.b
+    def op_add(self, x, y):
+        return x + y
 
-    def op_sub(self):
-        return self.a - self.b
+    def op_sub(self, x, y):
+        return x - y
     
-    def op_mul(self):
-        return self.a * self.b
+    def op_mul(self, x, y):
+        return x * y
 
-    def op_div(self):
-        return self.a / self.b
+    def op_div(self, x, y):
+        return x / y
 
 if __name__ == '__main__':
-    obj_cal = calculator_api(3, 4)
-    a = obj_cal.op_add()
-    b = obj_cal.op_sub()
-    c = obj_cal.op_mul()
-    d = obj_cal.op_div()
+    obj_cal = calculator_api()
+    a = obj_cal.op_add(4,3)
+    b = obj_cal.op_sub(10,11)
+    c = obj_cal.op_mul(5, 6)
+    d = obj_cal.op_div(8,9)
 
     print (f'Result ADD={a}')
     print (f'Result SUB={b}')
