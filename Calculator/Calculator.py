5f180e3f6c299682a186142a0a874839d13ace2e
Sarah <Sarah@cimiytech.com>
2024-06-26 15:36:06 +0800

    Add DIV function

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -15,15 +15,18 @@ class calculator_api:
     def op_mul(self):
         return self.a * self.b
 
+    def op_div(self):
+        return self.a / self.b
 
 if __name__ == '__main__':
     obj_cal = calculator_api(3, 4)
     a = obj_cal.op_add()
     b = obj_cal.op_sub()
     c = obj_cal.op_mul()
+    d = obj_cal.op_div()
+
     print (f'Result ADD={a}')
     print (f'Result SUB={b}')
     print (f'Result MUL={c}')
-
-
-
+    print (f'Result DIV={d}')
+    
